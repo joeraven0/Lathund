cf69e8c841449028a3918f821770aeebd21aadb5
joeraven0 <joakim.ringstad.raavi@gmail.com>
2017-12-19 13:44:01 +0100

    Delete loop.php

--- loop.php
+++
@@ -1,6 +0,0 @@
-#Post-loop by category
-<?php query_posts( 'category_name=CATEGORY-HERE&posts_per_page=10' ); ?>
-<?php while ( have_posts() ) : the_post(); ?>
-  <h1><?php the_title(); ?></h1>
-  <?php the_content(); ?>
-<?php endwhile; ?>
