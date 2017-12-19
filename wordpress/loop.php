7abe430fca0312f19924a879c2dbb2e9e9085bc8
joeraven0 <joakim.ringstad.raavi@gmail.com>
2017-12-19 16:38:13 +0100

    Update loop.php

--- wordpress/loop.php
+++ wordpress/loop.php
@@ -1,6 +1,17 @@
-<-- SHOW POST BY CATEGORY !-->
-<?php query_posts( 'category_name=CATEGORY-NAME&posts_per_page=10' ); ?>
-<?php while ( have_posts() ) : the_post(); ?>
-  <h1><?php the_title(); ?></h1>
-  <?php the_content(); ?>
-<?php endwhile; ?>
+<!--SHOW DIFFERENT CONTENT ON DIFFERENT PAGES-->
+<!--The page content -->
+<?php 
+  if(have_posts()){                                                
+    while (have_posts()){                                               
+    the_post();                                                             
+    the_content();                                                         
+    }
+  }
+?>
+<!--The post content, only categories with same name as page title-->
+<?php $txt = "category_name=".get_the_title($post)."&posts_per_page=10"; ?> <!--String defines which post category-->
+  <?php query_posts($txt); ?>                                               <!--Publish post content-->
+  <?php while(have_posts()):the_post(); ?>                                 
+    <h1><?php the_title(); ?></h1>                                          <!--Print post title-->
+    <?php the_content(); ?>                                                 <!--Print post content-->
+  <?php endwhile; ?>                                                      
